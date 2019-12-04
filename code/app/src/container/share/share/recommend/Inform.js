e1285a416d8832e9c68edca71789ea75f12ece2e
huangbilian <2650424700@qq.com>
2019-12-04 19:54:41 +0800

    add share

--- code/app/src/container/share/share/recommend/Inform.js
+++ code/app/src/container/share/share/recommend/Inform.js
@@ -11,6 +11,24 @@ export default class Inform extends Component {
     constructor(props){
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            data:[
+                {
+                    pic:'inform2.png',
+                    name:'专注睡眠一百年',
+                    date:'11-27',
+                    time:'02:32',
+                    comment:'评论：好好睡觉'
+                },
+                {
+                    pic:'inform2.png',
+                    name:'专注睡眠一百年',
+                    date:'11-27',
+                    time:'02:32',
+                    comment:'动态：熬夜的危害'
+                },
+            ]
+        }
     }
     handleClick(){
         this.props.history.push('/share');
@@ -18,11 +36,44 @@ export default class Inform extends Component {
     render() {
         return (
             <div style={{backgroundColor:'#fff'}}>
-                <div style={{border:'1px solid #d5d5d9',height:'30px'}}>
-                    <img src="./images/share/back.png" style={{width:'8%',height:'80%'}}  onClick={this.handleClick} />
+                <NavBar style={{backgroundColor:'#fff',border:'1px solid #d8d8d8'}}  leftContent={[
+                            <img src="./images/share/back.png" style={{width:'15%',height:'40%'}}  onClick={this.handleClick} />
+                ]}>
+                    <Tabs tabs={tabs}  tabBarTextStyle={{fontSize:'95%'}} initialPage={0}  tabBarBackgroundColor=''  tabBarInactiveTextColor='#8fa0cb' tabBarActiveTextColor='#8fa0cb'  tabBarUnderlineStyle={{border:'2px solid #8fa0cb'}} >
+                        {/* <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            通知
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            评论
+                        </div> */}
+                    </Tabs>
+                </NavBar>
+                <div>
+                    {
+                        this.state.data.map((item,idx)=>(
+                            <div style={{backgroundColor:'#fff',float:'left',marginBottom:'1px',width:'100%',maxHeight:'60px'}}>
+                                <div style={{marginTop:'1%',marginBottom:'1%'}}>
+                                    <img src={'./images/share/'+item.pic} style={{width:'50px',height:'50px',borderRadius:'50%',float:'left'}} />
+                                    
+                                    <a style={{paddingtop:'5px',marginLeft:"5px"}}>
+                                        {item.name}
+                                    </a> 
+                                    <a style={{fontSize:'60%',color:'gray',marginLeft:'15px'}}>
+                                        赞了你
+                                    </a>
+                                    <a style={{fontSize:'60%',color:'gray',marginLeft:'50px'}}>
+                                        {item.date} {item.time}
+                                    </a><br/>
+                                    <a style={{fontSize:"90%",marginLeft:'5px'}}>
+                                      {item.comment}
+                                    </a>
+                               </div>
+                               
+                            </div>
+                        ))
+                    }
                 </div>
-                <Tabs tabs={tabs}  tabBarTextStyle={{fontSize:'95%'}} initialPage={0}  tabBarBackgroundColor=''  tabBarInactiveTextColor='#8fa0cb' tabBarActiveTextColor='#8fa0cb'  tabBarUnderlineStyle={{border:'2px solid #8fa0cb'}} 
-                ></Tabs>
+                
                 
             </div>
         )
