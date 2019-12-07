83d74d7ae05b08157e8b0edaa0dedd8bb451e245
王梦 <33738315+wangmeng456@users.noreply.github.com>
2019-12-07 15:55:28 +0800

    Delete index.js

--- code/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import 'antd-mobile/dist/antd-mobile.css';
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
