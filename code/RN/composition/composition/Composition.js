7aaa8679310aa00101181e44c79eeb905222587b
wangmeng456 <3197985967@qq.com>
2020-05-28 20:33:58 +0800

    short-word

--- code/RN/composition/composition/Composition.js
+++ code/RN/composition/composition/Composition.js
@@ -16,8 +16,12 @@ let Y = date.getFullYear();
 let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
 let D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate());
 let groomdate = Y + '-' + M + '-' + D;
-let groomdate1 = Y + '/' + M + '/' + D;
+let groomdate1 = date.getMonth() + 1 + '月';
 console.log(groomdate);
+var a = ["日", "一", "二", "三", "四", "五", "六"];  
+var week = new Date().getDay();  
+var str = "星期"+ a[week];  
+console.log(str);
 
 export default class Composition extends Component {
     constructor(props) {
@@ -32,7 +36,12 @@ export default class Composition extends Component {
             banner_data3: [],
             banner_data4: [],
             banner_data5: [],
-            refreshing:false
+            refreshing: false,
+            short_data: [], 
+            backgroundColor1: '',
+            backgroundColor2: '',
+            color: '',
+            week: ''
         }
     }
     componentDidMount() {
@@ -44,6 +53,7 @@ export default class Composition extends Component {
                     this.setState({ uid: res })
                 this.fetchf()
                 this.banner_data()
+                this.short()
             })
         setTimeout(() => {
             this.setState({
@@ -214,24 +224,46 @@ export default class Composition extends Component {
                     banner_data4: res.data[3],
                     banner_data5: res.data[4],
                 });
-                console.log(this.state.banner_data);
             })
     }
     fetchf = () => {
         this.setState({
-            refreshing:true
-        },()=>{
+            refreshing: true
+        }, () => {
             fetch('http://116.62.14.0:8402/groom/time/' + groomdate)
+                .then((res) => res.json())
+                .then((res) => {
+                    this.setState({
+                        data: res.data,
+                        refreshing: false
+                    });
+                })
+        })
+
+    }
+    short = () => {
+        fetch('http://116.62.14.0:8402/short/date/'+groomdate)
             .then((res) => res.json())
             .then((res) => {
                 this.setState({
-                    data: res.data,
-                    refreshing:false
-                });
-                console.log(res.data);
+                    short_data: res.data,
+                });  
             })
-        })
-        
+            if(str === '星期一'){
+                this.setState({backgroundColor1:'#a5ace2',backgroundColor2:'#808bcd',color:'#626bb0',week:'MON'})
+            } else if(str === '星期二') {
+                this.setState({backgroundColor1:'#7fb5f1',backgroundColor2:'#578ece', color:'#487bb4',week:'TUE'})  
+            }else if(str === '星期三') {
+                this.setState({backgroundColor1:'#93d7ec',backgroundColor2:'#56aacc', color:'#3d91b5',week:'WED'})  
+            }else if(str === '星期四') {
+                this.setState({backgroundColor1:'#8de0e4',backgroundColor2:'#52c9cd', color:'#24a9b8',week:'THU'})  
+            }else if(str === '星期五') {
+                this.setState({backgroundColor1:'#92d8a2',backgroundColor2:'#63b575', color:'#4b9d5d',week:'FRI'})  
+            }else if(str === '星期六') {
+                this.setState({backgroundColor1:'#efbf81',backgroundColor2:'#e59c67', color:'#d68242',week:'SAT'})  
+            }else {
+                this.setState({backgroundColor1:'#f4af85',backgroundColor2:'#e58964', color:'#d86a45',week:'SUN'})  
+            }
     }
     render() {
         return (
@@ -245,67 +277,79 @@ export default class Composition extends Component {
                     </TouchableOpacity>
                 </View>
                 <ScrollView>
-                {/* 轮播 */}
-                <View style={styles.container}>
-                    {this.renderBanner()}
-                </View>
-                {/* tab */}
-                <View style={{ flexDirection: 'row', justifyContent: 'center',  backgroundColor: '#fff', alignItems: 'flex-end', paddingBottom: 30 * s, paddingTop: 20 * s }}>
-                    <View style={{ width: '80%', flexDirection: 'row', justifyContent: 'space-between', }}>
-                        <TouchableOpacity onPress={() => { Actions.source() }}>
-                            <Image source={require('../../assets/composition/composition/composition.png')} style={{ marginLeft: 14 * s }} />
-                            <Text>素材分类</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { Actions.skill1() }}>
-                            <Image source={require('../../assets/composition/composition/jifa.png')} style={{ marginLeft: 14 * s }} />
-                            <Text>技法学习</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { Actions.realpaper() }}>
-                            <Image source={require('../../assets/composition/composition/zhenti.png')} style={{ marginLeft: 14 * s }} />
-                            <Text>真题解析</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { Actions.collection() }}>
-                            <Image source={require('../../assets/composition/composition/heji.png')} style={{ marginLeft: 14 * s }} />
-                            <Text>日更合集</Text>
-                        </TouchableOpacity>
+                    {/* 轮播 */}
+                    <View style={styles.container}>
+                        {this.renderBanner()}
                     </View>
-                </View>
-                {/* 日更 */}
-                <View style={{ flexDirection: 'row', backgroundColor: '#FFF', justifyContent: 'space-between', marginTop: 12 * s, padding: 20 * s, borderBottomColor: 'gray', borderBottomWidth: s }}>
-                    <View><Text onPress={()=>{this.fetchf()}} style={{ fontSize: 26 * s }}>每日推荐</Text></View>
-                    <View><Text style={{ fontSize: 20 * s, color: 'gray', marginTop: 4 * s }}>{groomdate1}</Text></View>
-                </View>
-                <View>
-                    <FlatList
-                        style={{ backgroundColor: '#fff', paddingBottom: 100 * s }}
-                        data={this.state.data}
-                        numColumns={1}
-                        onRefresh = {()=>{
-                            this.fetchf()
-                        }}
-                        refreshing = { this.state.refreshing }
-                        renderItem={({ item }) => (
-                            item.mimage === '' ?
-                                <View style={{ justifyContent: 'center', flexDirection: 'row', marginTop: 20 * s }}>
-                                    <TouchableOpacity onPress={() => Actions.popular({ mid: item.mid })} style={{ padding: 10 * s, width: '90%', borderStyle: 'dashed', borderColor: 'gray', borderWidth: s, borderRadius: 14 * s }}>
-                                        <Text style={{ fontSize: 26 * s, margin: 10 * s, paddingTop: 20 * s }}>{item.mtitle}</Text>
-                                        <Text style={{ textAlign: 'right', fontSize: 18 * s, margin: 10 * s, color: 'gray' }}>{item.mlocal}</Text>
-                                    </TouchableOpacity>
-                                </View>
-                                :
-                                <View style={{ justifyContent: 'center', flexDirection: 'row', marginTop: 20 * s }}>
-                                    <TouchableOpacity onPress={() => Actions.popular({ mid: item.mid })} style={{ padding: 10 * s, width: '90%', borderStyle: 'dashed', borderColor: 'gray', borderWidth: s, borderRadius: 14 * s }}>
-                                        <Image
-                                            style={{ width: "100%", height: 240 * s }}
-                                            source={{ uri: 'http://116.62.14.0:8402/images/' + item.mimage }}
-                                        />
-                                        <Text style={{ fontSize: 26 * s }}>{item.mtitle}</Text>
-                                        <Text style={{ textAlign: 'right', fontSize: 18 * s, margin: 10 * s, color: 'gray' }}>{item.mlocal}</Text>
-                                    </TouchableOpacity>
-                                </View>
-                        )}
-                    />
-                </View>
+                    {/* tab */}
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', backgroundColor: '#fff', alignItems: 'flex-end', paddingBottom: 30 * s, paddingTop: 20 * s }}>
+                        <View style={{ width: '80%', flexDirection: 'row', justifyContent: 'space-between', }}>
+                            <TouchableOpacity onPress={() => { Actions.source() }}>
+                                <Image source={require('../../assets/composition/composition/composition.png')} style={{ marginLeft: 14 * s }} />
+                                <Text>素材分类</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => { Actions.skill1() }}>
+                                <Image source={require('../../assets/composition/composition/jifa.png')} style={{ marginLeft: 14 * s }} />
+                                <Text>技法学习</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => { Actions.realpaper() }}>
+                                <Image source={require('../../assets/composition/composition/zhenti.png')} style={{ marginLeft: 14 * s }} />
+                                <Text>真题解析</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => { Actions.collection() }}>
+                                <Image source={require('../../assets/composition/composition/heji.png')} style={{ marginLeft: 14 * s }} />
+                                <Text>日更合集</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    {/* 日更 */}
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 20 * s }}>
+                        <View><Text onPress={() => { this.fetchf() }} style={{ fontSize: 30 * s }}>每日推荐</Text></View>
+                    </View>
+                    {/* 一句话 */}
+                    <View style={{ position: 'relative', marginTop: 20 * s, marginBottom: 10 * s }}>
+                        <View style={{ position: 'absolute', width: 60 * s, height: 70 * s, backgroundColor: this.state.backgroundColor1, left: '7%', top: -20 * s, zIndex: 1, justifyContent: 'center', alignItems: 'center',borderRadius:6*s }}>
+                            <Text style={{ fontSize: 24 * s, color: '#fff' }}>{groomdate1}</Text>
+                            <Text style={{ fontSize: 30 * s, color: '#fff' }}>{D}</Text>
+                        </View>
+                        <View style={{ width: '90%', marginLeft: '5%', backgroundColor: this.state.backgroundColor2, height: 60 * s,borderRadius:16*s }}>
+                            <Text style={{ fontSize: 24 * s, lineHeight: 60 * s, color: '#fff', marginLeft: '16%' }}>{this.state.short_data.sentence}</Text>
+                        </View>
+                        <View style={{ position: 'absolute',left: '74%',top: 12*s}}>
+                            <Text style={{ fontSize: 40 * s, lineHeight: 60 * s, marginLeft: '16%',color:this.state.color }}>{this.state.week}</Text>
+                        </View>
+                    </View>
+                    <View>
+                        <FlatList
+                            style={{ paddingBottom: 100 * s }}
+                            data={this.state.data}
+                            numColumns={1}
+                            onRefresh={() => {
+                                this.fetchf()
+                            }}
+                            refreshing={this.state.refreshing}
+                            renderItem={({ item }) => (
+                                item.mimage === '' ?
+                                    <View style={{ justifyContent: 'center', flexDirection: 'row', marginTop: 20 * s }}>
+                                        <TouchableOpacity onPress={() => Actions.popular({ mid: item.mid })} style={{ padding: 10 * s, width: '90%', backgroundColor: '#fff' }}>
+                                            <Text style={{ fontSize: 26 * s, margin: 10 * s, paddingTop: 20 * s }}>{item.mtitle}</Text>
+                                            <Text style={{ textAlign: 'right', fontSize: 18 * s, margin: 10 * s, color: 'gray' }}>{item.mlocal}</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                    :
+                                    <View style={{ justifyContent: 'center', flexDirection: 'row', marginTop: 20 * s }}>
+                                        <TouchableOpacity onPress={() => Actions.popular({ mid: item.mid })} style={{ padding: 10 * s, width: '90%', backgroundColor: '#fff' }}>
+                                            <Image
+                                                style={{ width: "100%", height: 240 * s }}
+                                                source={{ uri: 'http://116.62.14.0:8402/images/' + item.mimage }}
+                                            />
+                                            <Text style={{ fontSize: 26 * s, marginTop: 10 * s }}>{item.mtitle}</Text>
+                                            <Text style={{ textAlign: 'right', fontSize: 18 * s, margin: 10 * s, color: 'gray' }}>{item.mlocal}</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                            )}
+                        />
+                    </View>
                 </ScrollView>
             </View>
         )
@@ -317,8 +361,8 @@ const styles = StyleSheet.create({
         height: 280 * s,
         width: '100%',
         paddingLeft: '5%',
-        paddingRight:'5%',
-        backgroundColor:'#fff'
+        paddingRight: '5%',
+        backgroundColor: '#fff'
     },
     wrpaper: {
         height: 280 * s,
